8edbc727f3ec5efc7eaeb0a93fd0b37f4dfb68f3
Brandon Eliott Hansen <bhansen4@dsg1.crc.nd.edu>
2018-03-22 11:36:33 -0400

    minor changes to htmlparser

--- htmlparser.py
+++ htmlparser.py
@@ -1,13 +1,5 @@
 from html.parser import HTMLParser
 import pandas as pd
-
-in_file = 'vegetable.csv'
-out_file = 'recipe_example.txt'
-what_do_out = 'w'#write 'w' or append 'a', write writes over append just appends to bottom
-
-#open file for writing
-f = open(out_file, what_do_out)
-
 # create a subclass and override the handler methods
 #https://docs.python.org/2/library/htmlparser.html
 class MyHTMLParser(HTMLParser):
@@ -22,18 +14,35 @@ class MyHTMLParser(HTMLParser):
 		f.write(data+'\n')
 
 
-#open csv file in pandas, usecols only gets columns defined,
-#    add other column names like usecols=['category', 'title', 'info']
-df = pd.read_csv(in_file, usecols=['info'])
+def parsehtmlfile(in_file):
 
-#get particular column info by itself
-info = df['info']
+	out_file = 'recipes.txt'
+	what_do_out = 'a'#write 'w' or append 'a', write writes over append just appends to bottom
 
-#loop through all and parse and put into file
-length = len(info)
-for index in range(0, length):
-	parser = MyHTMLParser()
-	parser.feed(info[index])
-	f.write('\n')
+	#open file for writing
+	f = open(out_file, what_do_out)
 
-f.close()
+
+	#open csv file in pandas, usecols only gets columns defined,
+	#    add other column names like usecols=['category', 'title', 'info']
+	df = pd.read_csv(in_file, usecols=['info'])
+
+	#get particular column info by itself
+	info = df['info']
+
+	#loop through all and parse and put into file
+	length = len(info)
+	for index in range(0, length):
+		parser = MyHTMLParser()
+		parser.feed(info[index])
+		f.write('\n')
+
+	f.close()
+
+ins = ['appetizers.csv', 'cake.csv', 'dips.csv', 'nuts.csv', 'sushi.csv', 'beverage.csv', 'christmas.csv', 'favorites.csv', 'pasta.csv', 'thanksgiving.csv', 'breads.csv', 'dinnerparty.csv', 'healthy.csv', 'quickandeasy.csv', 'vegetable.csv']
+
+for nm in ins:
+	parsehtmlfile('recipedata/'+nm)
+
+
+
