a9c4b01c6fd6db68b85eb59a2763714f0a17af19
Brandon Eliott Hansen <bhansen4@dsg1.crc.nd.edu>
2018-03-28 10:39:20 -0400

    changed insert file name

---
+++ insertdb_recipe.py
@@ -0,0 +1,37 @@
+import MySQLdb
+import time
+
+
+fs = ['recipe_df.txt']
+
+bad_char = []
+
+conn = MySQLdb.connect(host='localhost',
+			user='bhansen4',
+			passwd='pw',
+			db="bhansen4")
+
+x = conn.cursor()
+
+id_count = 0
+
+for f in fs:
+	with open(f, 'r') as fi:
+		for line in fi:
+			row = line
+			for char in bad_char:
+				row = row.replace(char, '')
+			split_row = row.split('<!><!><!>');
+			name = split_row[0]
+			desc = split_row[1]
+			nutr = split_row[2].strip()
+			try:
+				x.execute("""INSERT INTO recipe VALUES (%s,%s,%s,%s)""", (name, desc, nutr, id_count))
+				id_count = id_count + 1
+				conn.commit()
+				print('insert '+name+' success')
+			except:
+				print('insert '+name+' fail')
+				conn.rollback()
+
+conn.close()
